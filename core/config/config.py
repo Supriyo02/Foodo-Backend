bb7982f913e059d25a1a2af1dac1e8d0eb8d58ce
Supriyo02 <supriyoghosh27125@gmail.com>
2025-09-09 22:15:15 +0530

    change in ci pipeline

--- core/config/config.py
+++ core/config/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     DATABASE_URL: str
-    ROUTER_PREFIX: str
+    ROUTER_PREFIX: str = "/api/v1"
     TOKEN_SECRET_KEY: str
     TOKEN_ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
